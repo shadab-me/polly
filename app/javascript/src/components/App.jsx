4e6c8765b2cadcadf53dd8d12b2e510ec0263ffe
= <=>
2021-04-03 11:02:27 +0530

    Added reusable components

--- app/javascript/src/components/App.jsx
+++ app/javascript/src/components/App.jsx
@@ -1,19 +1,16 @@
-import Logger from "js-logger";
 import React, { useEffect } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
 const App = () => {
   useEffect(() => {
     initializeLogger();
-    Logger.info("log from js logger");
+    logger.info("Log from babel js-logger");
   }, []);
+
   return (
     <Router>
       <Switch>
-        <Route
-          exact
-          path="/"
-          render={() => <div className="bg-blue-900 font-weight-900">Home</div>}
-        />
+        <Route exact path="/" render={() => <div>Home</div>} />
         <Route exact path="/about" render={() => <div>About</div>} />
       </Switch>
     </Router>
